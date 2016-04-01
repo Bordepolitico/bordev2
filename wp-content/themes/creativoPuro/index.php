41d034dcaa20ef2e5a099e12e58098f5d76c8fc7
Rene-gade <renetapialopez@gmail.com>
2016-04-01 10:14:26 -0600

    borde beta

--- wp-content/themes/creativoPuro/index.php
+++ wp-content/themes/creativoPuro/index.php
@@ -1,6 +1,6 @@
-<?php include($_SERVER['DOCUMENT_ROOT'] ."../../../header.php") ?>
-<?php include($_SERVER['DOCUMENT_ROOT'] ."../../../global/menu.php") ?>
-<?php echo $_SERVER['DOCUMENT_ROOT'] ."../../../header.php" ?>
+<?php include($_SERVER['DOCUMENT_ROOT'] ."/bordef/bordev2/header.php") ?>
+<?php include($_SERVER['DOCUMENT_ROOT'] ."/bordef/bordev2/global/menu.php") ?>
+<?php echo $_SERVER['DOCUMENT_ROOT'] ."/bordef/bordev2/header.php" ?>
 <div id="home" ng-controller="homeController" flex layout="row" layout-align="start center" layout-wrap>
 	<div id="slider-home" class="home-cont" flex="100">
 		<div class="item" flex="100" layout="row" layout-wrap>
@@ -454,5 +454,5 @@
 	</div>
 </div>
 <div flex="100">
-<?php include($_SERVER['DOCUMENT_ROOT'] ."../../../footer.php") ?>
+<?php include($_SERVER['DOCUMENT_ROOT'] ."/bordef/bordev2/footer.php") ?>
 </div>
